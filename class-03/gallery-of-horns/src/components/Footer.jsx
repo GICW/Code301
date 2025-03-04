5d4423beff1a42d919f3c035d1aa4a4f60ef52e1
Qilin Xie <qxie@gicw.org>
2025-03-04 07:44:40 -0800

    solution lab3

---
+++ class-03/gallery-of-horns/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import Navbar from 'react-bootstrap/Navbar';
+
+function Footer() {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">Author: Your name</Navbar.Brand>
+    </Navbar>
+  );
+}
+
+export default Footer;
